b27d3497fa0401204930e5f51745aa48aa1975bf
TheKingsCrown <Stevenmars77@gmail.com>
2023-11-12 21:11:52 -0800

    Initial commit

---
+++ src/main/java/tkc/paleontologistsdream/PaleontologistsDream.java
@@ -0,0 +1,17 @@
+package tkc.paleontologistsdream;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PaleontologistsDream implements ModInitializer {
+
+	public static final String MOD_ID = "paleontologists-dream";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/tkc/paleontologistsdream/PaleontologistsDreamClient.java
@@ -0,0 +1,10 @@
+package tkc.paleontologistsdream;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PaleontologistsDreamClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
